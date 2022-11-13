df40fcd33f61745289902204808d5864a77e954b
Alex Rote <level23archbard@gmail.com>
2022-11-13 14:33:28 -0600

    Making the dice rolling a little fancier

--- src/app/dice/dice.component.ts
+++ src/app/dice/dice.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { delay, Observable, of, Subject, Subscription, switchMap, tap } from 'rxjs';
 
 import { Dice, DiceResult, DiceService } from './dice.service';
 
@@ -7,17 +8,33 @@ import { Dice, DiceResult, DiceService } from './dice.service';
   templateUrl: './dice.component.html',
   styleUrls: ['./dice.component.scss'],
 })
-export class DiceComponent implements OnInit {
+export class DiceComponent implements OnInit, OnDestroy {
 
   @Output() closeEvent = new EventEmitter<void>();
-
+  private subscriptions = new Subscription();
+  private diceRoll = new Subject<Dice>();
   dice: Dice[] = [];
   activeResult: DiceResult | null = null;
+  rollingText = '';
 
   constructor(private diceService: DiceService) {}
 
   ngOnInit(): void {
     this.dice = this.diceService.standardDiceArray;
+
+    this.subscriptions.add(
+      this.diceRoll.pipe(
+        switchMap((dice) => this.diceService.rollDice(dice)),
+        switchMap((result) => this.simulateRoll(result)),
+      ).subscribe((result) => {
+        this.activeResult = result;
+        this.rollingText = '';
+      }),
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   onClickClose(): void {
@@ -25,8 +42,28 @@ export class DiceComponent implements OnInit {
   }
 
   onClickDice(dice: Dice): void {
-    this.diceService.rollDice(dice).subscribe((result) => {
-      this.activeResult = result;
-    });
+    this.diceRoll.next(dice);
+  }
+
+  private simulateRoll(result: DiceResult): Observable<DiceResult> {
+    return of(result).pipe(
+      tap(() => {
+        this.activeResult = null;
+        this.rollingText = 'Rolling';
+      }),
+      delay(50),
+      tap(() => {
+        this.rollingText = 'Rolling.';
+      }),
+      delay(50),
+      tap(() => {
+        this.rollingText = 'Rolling..';
+      }),
+      delay(50),
+      tap(() => {
+        this.rollingText = 'Rolling...';
+      }),
+      delay(50),
+    );
   }
 }
